07def4d5b69566885f9bb7deba3170328c602e26
Salmaan <salmaan.arif.ali@gmail.com>
2021-06-18 01:48:51 -0400

    commit tree 92aab5cfe0e561ccaef0cc27ee3c8a770f9cb57e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumber = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumber, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumber = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumber, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  } 
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+
+})
